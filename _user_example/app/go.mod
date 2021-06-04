1877cd2ac3741fd7a7867e4acdd199fd9c0fed73
Julio Guerra <julio@datadog.com>
2021-06-04 10:47:42 +0200

    example demo app

---
+++ _user_example/app/go.mod
@@ -0,0 +1,5 @@
+module app-demo
+
+go 1.16
+
+require github.com/Julio-Guerra/tracer v1.0.0
---
+++ _user_example/app/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Julio-Guerra/tracer"
+
+func main() {
+	tracer.Hello()
+}
